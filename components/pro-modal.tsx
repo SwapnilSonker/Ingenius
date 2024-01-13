a26df123c068115b7e2b3d58f6fb420a190c44ed
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-14 01:20:26 +0530

    Error fixed - deploy

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -11,7 +11,7 @@ import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
-export const tools = [
+const tools = [
     {
     label: "Conversation",
     icon: MessageSquare,
