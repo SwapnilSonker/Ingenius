d9a9ae5e8ed99f86b8b47a5f14a82f86ed4d3c52
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-17 23:16:31 +0530

    fixed the type error on music

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -32,7 +32,7 @@ export async function POST(
             return new NextResponse("Free trial has expired", { status: 403 });
         }
 
-        const output = await replicate.run(
+        const output: any = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
               input: {
